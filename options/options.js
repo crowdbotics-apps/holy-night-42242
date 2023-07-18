9ec813d288fb8381c85e4c0f49a3b2890649378e
Crowdbotics <team@crowdbotics.com>
2023-07-18 03:39:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-night-42242",
+  url: "https://holy-night-42242.botics.co",
+  api: "https://holy-night-42242.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
